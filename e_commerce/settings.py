6ea918eb35d08c94b8f9a9e8fd2c0aa078e2bf9f
rozzy rahmanda <rozzyrahmanda0@gmail.com>
2020-06-16 09:31:18 +0700

    Heroku upload

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'crispy_forms',
     'django.contrib.humanize',
@@ -128,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Custom user model
 AUTH_USER_MODEL = 'users.Account'
